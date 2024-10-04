afd46d9770b49ce899b2c5378d1c19c75dfb038d
Bruce Ho <mitch6198@gmail.com>
2024-10-04 17:45:34 +0800

    Add new file

--- 01.py
+++ 01.py
@@ -1,4 +1,15 @@
-import random
+data = []
+count = 0 
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案已讀取完,共有',len (data),'筆資料')
 
-r = random.randint(1, 100)
-print(r)
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('平均是',sum_len/len(data))
